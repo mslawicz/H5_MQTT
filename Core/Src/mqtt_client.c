c28a3125a3a66e1bece5f8fc91449ba0753acb4f
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-05-29 11:53:44 +0200

    mqtt setup #2 (no DNS)

--- Core/Src/mqtt_client.c
+++ Core/Src/mqtt_client.c
@@ -1,45 +1,6 @@
 #include "mqtt_client.h"
-#include "main.h"
-#include "nx_api.h"
-#include "tx_api.h"
-#include "nx_stm32_eth_driver.h"
 
-#define NX_PACKET_PAYLOAD_SIZE  1536
-#define NX_PACKET_POOL_SIZE ((NX_PACKET_PAYLOAD_SIZE + sizeof(NX_PACKET)) * 10)
-#define NETMASK 0xFFFFFF00UL // 255.255.255.0
-#define NX_IP_SIZE  2048
-
-NX_PACKET_POOL nx_packet_pool;
-NX_IP ip_0;
-//NX_DNS dns_0;
-
-void nx_application_define(void *first_unused_memory)
+void dummy_function(void)
 {
-    UINT status;
 
-    /* Initialize the NetX system. */
-    nx_system_initialize();
-
-    /* Create a packet pool. */
-    status = nx_packet_pool_create(&nx_packet_pool, "Packet Pool", NX_PACKET_PAYLOAD_SIZE, first_unused_memory, NX_PACKET_POOL_SIZE);
-    if(status != NX_SUCCESS)
-    {
-        Error_Handler();
-    }
-    
-    /* Create an IP instance. */
-    nx_ip_create(&ip_0, "IP Instance 0", IP_ADDRESS(192, 168, 1, 101), NETMASK, &nx_packet_pool, nx_stm32_eth_driver, first_unused_memory, NX_IP_SIZE, 1);
-    
-    // Enable ARP, UDP, TCP, ICMP
-    //nx_arp_enable(&ip_0, ARP_CACHE, ARP_CACHE_SIZE);
-    //nx_udp_enable(&ip_0);
-    //nx_tcp_enable(&ip_0);
-    //nx_icmp_enable(&ip_0);
-    
-    // DNS initialization
-    //nx_dns_create(&dns_0, &ip_0, (UCHAR *)"DNS Instance");
-    //nx_dns_server_add(&dns_0, IP_ADDRESS_DNS_SERVER);
-    
-    // Start network
-    //nx_ip_address_set(&ip_0, IP_ADDRESS, NETMASK);
 }
--- NetXDuo/App/app_netxduo.c
+++ NetXDuo/App/app_netxduo.c
@@ -33,7 +33,11 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define NX_PACKET_PAYLOAD_SIZE  1536
+#define NX_PACKET_POOL_SIZE ((NX_PACKET_PAYLOAD_SIZE + sizeof(NX_PACKET)) * 10)
+#define NETMASK 0xFFFFFF00UL // 255.255.255.0
+#define NX_IP_SIZE  2048
+#define ARP_CACHE_SIZE 1024
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,6 +47,9 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
+NX_PACKET_POOL nx_packet_pool;
+NX_IP ip_0;
+//NX_DNS dns_0;
 
 /* USER CODE END PV */
 
@@ -66,6 +73,41 @@ UINT MX_NetXDuo_Init(VOID *memory_ptr)
   /* USER CODE END App_NetXDuo_MEM_POOL */
   /* USER CODE BEGIN 0 */
 
+    /* Initialize the NetX system. */
+    nx_system_initialize();
+
+    /* Create a packet pool. */
+    ret = nx_packet_pool_create(&nx_packet_pool, "Packet Pool", NX_PACKET_PAYLOAD_SIZE, memory_ptr, NX_PACKET_POOL_SIZE);
+    if(ret != NX_SUCCESS)
+    {
+        return ret;
+    }
+    
+    /* Create an IP instance. */
+    ret = nx_ip_create(&ip_0, "IP Instance 0", IP_ADDRESS(192, 168, 1, 101), NETMASK, &nx_packet_pool, nx_stm32_eth_driver, memory_ptr, NX_IP_SIZE, 1);
+    if(ret != NX_SUCCESS)
+    {
+        return ret;
+    }
+
+    /* Enable ARP and supply ARP cache memory. */
+    nx_arp_enable(&ip_0, memory_ptr, ARP_CACHE_SIZE);
+
+    /* Enable the ICMP. */
+    nx_icmp_enable(&ip_0);
+
+    /* Enable the UDP protocol. */
+    nx_udp_enable(&ip_0);
+
+    /* Enable the TCP protocol. */
+    nx_tcp_enable(&ip_0);
+    
+    /* DNS initialization */
+    ////nx_dns_create(&dns_0, &ip_0, (UCHAR *)"DNS Instance");
+    //nx_dns_server_add(&dns_0, IP_ADDRESS_DNS_SERVER);
+    
+    // Start network
+    //nx_ip_address_set(&ip_0, IP_ADDRESS, NETMASK);
   /* USER CODE END 0 */
 
   /* USER CODE BEGIN MX_NetXDuo_Init */
